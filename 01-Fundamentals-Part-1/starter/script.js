b8c851517daac1a8927437ab8564784512748d7d
sushmit1 <sushmit718@gmail.com>
2022-10-15 14:03:03 +0530

    js: some basics of variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,2 +1,14 @@
 let js = 'amazing';
-if (js == 'amazing') alert('JS is fun');
+
+console.log('redducc');
+
+/* variables moment
+   Don't start with numbers */
+
+let firstName = 'redducc';
+console.log(firstName);
+
+/* label constants with uppercase */
+
+let PI = 3.14;
+alert(PI);
