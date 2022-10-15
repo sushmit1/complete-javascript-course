71d98c197bc415c18ecba39aefbb81993d563c4e
sushmit1 <sushmit718@gmail.com>
2022-10-15 19:15:34 +0530

    js: Data types * Variables, reassigning variables and again reassigning it
    Co-authored-by: Dhruv <dhruvchhura18@gmail.com>
    Co-authored-by: Sourav <sourav@project404.xyz>

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,14 +1,37 @@
-let js = 'amazing';
+/* let js = 'amazing';
 
-console.log('redducc');
+// console.log('redducc');
 
-/* variables moment
-   Don't start with numbers */
+variables moment
+   Don't start with numbers
 
 let firstName = 'redducc';
 console.log(firstName);
 
-/* label constants with uppercase */
+//label constants with uppercase
 
 let PI = 3.14;
 alert(PI);
+*/
+
+let javascript = true;
+console.log(javascript);
+
+// console.log(typeof true);
+console.log(typeof javascript);
+// console.log(typeof 23);
+
+// Reassign new value to variables
+javascript = 'YES';
+console.log(typeof javascript);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+// Reassign pt 2
+year = 2005;
+alert(year);
+
+// ayo bug
+console.log(typeof null);
